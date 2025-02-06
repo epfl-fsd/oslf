719505a34d528652f6bac0197b04104744d65557
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-02-06 16:47:19 +0100

    [add] error handling for menu retrieval in MenuList component

--- src/components/menu/list.tsx
+++ src/components/menu/list.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import { Menu } from '@/types/menu';
 import { MenuItem } from './item';
+import { useTranslations } from 'next-intl';
+import { Error } from '../error';
+import { CircleAlert, Frown } from 'lucide-react';
 
-export const MenuList: React.FC<{ menus: Menu[] }> = ({ menus }) => {
+export const MenuList: React.FC<{ menus: Menu[] | null }> = ({ menus }) => {
+	const t = useTranslations('Home');
+
+	if (!menus) {
+		return <Error text={t('error.notFound')} subText={t('error.problem')} Icon={CircleAlert} color="text-red-500" />;
+	}
+
+	if (menus.length === 0) {
+		return <Error text={t('error.noMenus')} subText={t('error.tryAgain')} Icon={Frown} color="text-gray-500" />;
+	}
+
 	return (
 		<div className="text-primary-secondary space-y-3 mx-auto md:w-[60%] sm:w-[80%] w-full py-2 px-3 items-center justify-between">
 			{menus.map((menu, index) => (
