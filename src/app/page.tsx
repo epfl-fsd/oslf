6220fa1c06067b96bba5ddbd0554db668f4bceaf
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-02-06 16:47:23 +0100

    [add] integrate MenuList component to display cafeteria menus on the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
-import { getTranslations } from 'next-intl/server';
+import { Error } from '@/components/error';
+import { MenuList } from '@/components/menu/list';
+import { getMenus } from '@/services/menu';
+import { Menu } from '@/types/menu';
+import { CircleAlert } from 'lucide-react';
+import { useTranslations } from 'next-intl';
 
 export default async function Home() {
-	const t = await getTranslations('Home');
-	return <h1>{t('welcome')}</h1>;
+	const menus: Menu[] | null = await getMenus();
+
+	return <MenuList menus={menus} />;
 }
