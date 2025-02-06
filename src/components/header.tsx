dc94e98b6d663a68040ac1c1394418b430eed76c
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-02-06 14:30:17 +0100

    [add] implement Header and LanguageSelector components for locale selection

---
+++ src/components/header.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { LanguageSelector } from '@/components/language-selector';
+
+export const Header: React.FC = () => {
+	return (
+		<header className="text-primary-secondary sm:py-4 sm:px-6 py-2 px-3 flex items-center justify-between border-b-2 border-0">
+			<div className="flex items-center sm:gap-4 gap-3 sm:p-3 p-2">
+				<img src="https://epfl-si.github.io/elements/svg/epfl-logo.svg" alt="OUSONLESFRITES" className="sm:h-7 h-5" />
+				<span className="border-l-2 border-solid h-6 w-1 border-gray-300"></span>
+				<h1 className="sm:text-2xl text-xl font-semibold">OUSONLESFRITES</h1>
+			</div>
+			<div className="flex items-center gap-4">
+				<LanguageSelector />
+			</div>
+		</header>
+	);
+};
