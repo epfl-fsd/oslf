558b611625c7cc08dccf667e50b468dc0147f617
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-02-21 16:52:58 +0100

    [fix] update alt text for logo image in header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ export const Header: React.FC = () => {
 	return (
 		<header className="text-primary-secondary py-2 px-2 sm:py-4 sm:px-6 flex items-center justify-between border-b-2 border-0 select-none">
 			<div className="flex items-center gap-2 sm:gap-4 p-1 sm:p-3">
-				<img src="https://epfl-si.github.io/elements/svg/epfl-logo.svg" alt="OUSONTLESFRITES" className="h-3 sm:h-7" />
+				<img src="https://epfl-si.github.io/elements/svg/epfl-logo.svg" alt="LOGO" className="h-3 sm:h-7" />
 				<span className="border-l-2 border-solid sm:h-6 h-4 w-1 border-gray-300"></span>
 				<h1 className="text-base sm:text-2xl font-semibold -ml-1 sm:ml-0">OUSONTLESFRITES</h1>
 			</div>
