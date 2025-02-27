488ad4749e3edd451efb0add4167c62cd8d3d75e
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-02-27 14:12:36 +0100

    [refactor] remove unused imports from page component for cleaner code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import { Error } from '@/components/error';
 import { MenuList } from '@/components/menu/list';
 import { getMenus } from '@/services/menu';
 import { Menu } from '@/types/menu';
-import { CircleAlert } from 'lucide-react';
-import { useTranslations } from 'next-intl';
 
 export default async function Home() {
 	const menus: Menu[] | null = await getMenus();
