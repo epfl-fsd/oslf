8a7eb24c92521d754b8be59869612becc338f898
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-02-27 13:05:36 +0100

    [feat] update Footer component to link version and source code for better accessibility

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,10 @@ import packageJson from '../../package.json';
 export const Footer: React.FC = () => {
 	return (
 		<footer className="text-primary-secondary mt-auto py-4 px-6 text-gray-200 select-none flex justify-center items-center">
-			<span className="text-sm">Version {packageJson.version} - </span>
+			<a href={packageJson.repository.web + '/releases/tag/' + packageJson.version} target="_blank" rel="noopener noreferrer" className="hover:underline text-sm">
+				Version {packageJson.version}
+			</a>
+			<span className="text-sm"> - </span>
 			<a href={packageJson.repository.web} target="_blank" rel="noopener noreferrer" className="hover:underline text-sm">
 				Source code
 			</a>
