c8c30efa6907bf8d14334c6862f6041f49a006b9
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-02-28 11:33:50 +0100

    [fix] add gap between footer items for improved layout

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import packageJson from '../../package.json';
 
 export const Footer: React.FC = () => {
 	return (
-		<footer className="text-primary-secondary mt-auto py-4 px-6 text-gray-200 select-none flex justify-center items-center">
+		<footer className="text-primary-secondary mt-auto py-4 px-6 text-gray-200 gap-2 select-none flex justify-center items-center">
 			<a href={packageJson.repository.web + '/releases/tag/' + packageJson.version} target="_blank" rel="noopener noreferrer" className="hover:underline text-sm">
 				Version {packageJson.version}
 			</a>
